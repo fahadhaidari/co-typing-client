cbb1a82eea17938e29e9b86cd461e398c09b0456
Fahad Haidari <fahad.haidari@gmail.com>
2017-08-16 00:38:30 -0600

    multi users can edit the same document (the scope of the project)

--- app/story/story.controller.js
+++ app/story/story.controller.js
@@ -4,30 +4,38 @@
     .module("cotyping")
     .controller("StoryController", StoryController);
 
-    function StoryController(SharedService) {
-      console.log("Story Controller");
-      const vm = this;
-      vm.sharedService = SharedService;
+  function StoryController($scope, SharedService) {
+    console.log("Story Controller");
+    const vm = this;
+    vm.sharedService = SharedService;
 
-      vm.userName = vm.sharedService.getUserName();
-      vm.roomName = vm.sharedService.getRoomName();
+    vm.userName = vm.sharedService.getUserName();
+    vm.roomName = vm.sharedService.getRoomName();
 
-      console.log("I am the user " , vm.userName);
+    vm.inputText = "";
 
-      vm.keyUp = (event) => {
-        console.log("Typing ", event.keyCode);
-        let info = {
-          userName: vm.userName,
-          roomName: vm.roomName,
-          message: event.keyCode
-        }
-        socket.emit("message", info);
+    console.log("I user ", vm.userName);
+
+    vm.keyUp = () => {
+      console.log("Typing ", event.keyCode);
+      let info = {
+        userName: vm.userName,
+        roomName: vm.roomName,
+        // message: event.keyCode
+        message: vm.inputText
       }
-
-      socket.on("message", function(_info){
-        console.log("User ", _info.userName, " typed ", _info.message);
-      });
-
+      socket.emit("message", info);
     }
+
+    socket.on("message", function(_info) {
+      console.log("User ", _info.userName, " typed ", _info.message);
+      vm.inputText = _info.message;
+
+      $scope.$applyAsync(function() {
+        $scope.connected = 'TRUE';
+      });
+    });
+
+  }
 
 })();
